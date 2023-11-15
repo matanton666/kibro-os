6b4bb9843049fc664d513e34b104907c5e227d89
yonatanoss <yon2006.ossdon@gmnail.com>
2023-11-15 19:10:17 +0200

    added font struct

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -4,6 +4,20 @@
 #error "This is not a cross-compiler"
 #endif
 
+#define PSF_FONT_MAGIC 0x864ab572
+
+typedef struct {
+    uint32_t magic;         /* magic bytes to identify PSF */
+    uint32_t version;       /* zero */
+    uint32_t headersize;    /* offset of bitmaps in file, 32 */
+    uint32_t flags;         /* 0 if there's no unicode table */
+    uint32_t numglyph;      /* number of glyphs */
+    uint32_t bytesperglyph; /* size of each glyph */
+    uint32_t height;        /* height in pixels */
+    uint32_t width;         /* width in pixels */
+} PSF_font;
+
+
 extern "C" void kernel_main(void) {
 	while (true)
 	{
