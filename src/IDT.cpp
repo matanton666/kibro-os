53123a67543dcf46f964aec2eb4a1c640d957854
matan kabalo <matanton666@gmail.com>
2023-12-12 19:31:38 +0000

    added some ISRs and implemented them in a better way

--- src/IDT.cpp
+++ src/IDT.cpp
@@ -20,17 +20,43 @@ void idt_init()
     idtPtr.limit = (uint16_t)(sizeof(IdtEntry) * IDT_SIZE - 1);
     // memset((void *)idtPtr.base, 0, idtPtr.limit); //TODO: memset here when implemented
 
-    for (int i = 0; i < 40; i++)
-    { 
-        idtSetEntry(i, (void*)generalFault, IDT_INTERRUPT_GATE);
-    }
+    idtSetEntry(0x00, (void *)generalFault, IDT_INTERRUPT_GATE);
+    idtSetEntry(0x01, (void *)generalFault, IDT_INTERRUPT_GATE);
+    idtSetEntry(0x02, (void *)generalFault, IDT_INTERRUPT_GATE);
+
+    idtSetEntry(0x03, (void *)generalFault, IDT_TRAP_GATE);
+    idtSetEntry(0x04, (void *)generalFault, IDT_TRAP_GATE);
+
+    idtSetEntry(0x05, (void *)generalFault, IDT_INTERRUPT_GATE);
+    idtSetEntry(0x06, (void *)generalFault, IDT_INTERRUPT_GATE);
+    idtSetEntry(0x07, (void *)generalFault, IDT_INTERRUPT_GATE);
+    idtSetEntry(0x08, (void *)generalFaultWithErrCode, IDT_INTERRUPT_GATE);
+    idtSetEntry(0x09, (void *)generalFault, IDT_INTERRUPT_GATE);
+
+    idtSetEntry(0x0a, (void *)generalFaultWithErrCode, IDT_INTERRUPT_GATE);
+    idtSetEntry(0x0b, (void *)generalFaultWithErrCode, IDT_INTERRUPT_GATE);
+    idtSetEntry(0x0c, (void *)generalFaultWithErrCode, IDT_INTERRUPT_GATE);
+    idtSetEntry(0x0d, (void *)generalFaultWithErrCode, IDT_INTERRUPT_GATE);
+    idtSetEntry(0x0e, (void *)pagefaultHandler, IDT_INTERRUPT_GATE);
+
+    idtSetEntry(0x10, (void *)generalFault, IDT_INTERRUPT_GATE);
+    idtSetEntry(0x11, (void *)generalFaultWithErrCode, IDT_INTERRUPT_GATE);
+    idtSetEntry(0x12, (void *)generalFault, IDT_INTERRUPT_GATE);
+    idtSetEntry(0x13, (void *)generalFault, IDT_INTERRUPT_GATE);
+    idtSetEntry(0x14, (void *)generalFault, IDT_INTERRUPT_GATE);
+    idtSetEntry(0x15, (void *)generalFaultWithErrCode, IDT_INTERRUPT_GATE);
+
+    idtSetEntry(0x1b, (void *)generalFault, IDT_INTERRUPT_GATE);
+    idtSetEntry(0x1c, (void *)generalFault, IDT_INTERRUPT_GATE);
+    idtSetEntry(0x1d, (void *)generalFaultWithErrCode, IDT_INTERRUPT_GATE);
+    idtSetEntry(0x1e, (void *)generalFaultWithErrCode, IDT_INTERRUPT_GATE);
     idtSetEntry(0x0E, (void*)pagefaultHandler, IDT_INTERRUPT_GATE);
-    
+
 
     __asm__ volatile(
         "lidt %0" // Load the IDT
         :
         : "m"(idtPtr) 
     );
-    __asm__ volatile("sti");
+    // __asm__ volatile("sti");
 }
--- src/headers/interrupts.h
+++ src/headers/interrupts.h
@@ -1,8 +1,20 @@
 #pragma once
-
+/// these are the ISRs of the IDT
 #include "screen.h"
 
 
-struct interrupt_frame;
-__attribute__((interrupt)) void pagefaultHandler(struct interrupt_frame* frame);
-__attribute__((interrupt)) void generalFault(struct interrupt_frame* frame);
+struct InterruptFrame {
+    unsigned int ip;
+    unsigned int cs;
+    unsigned int flags;
+    unsigned int sp;
+    unsigned int ss;
+};
+
+// error code needs to be unsigned int because this is 32-bit os
+
+__attribute__((interrupt)) void generalFault(struct InterruptFrame* frame);
+__attribute__((interrupt)) void generalFaultWithErrCode(struct InterruptFrame *frame, unsigned int errorCode);
+
+__attribute__((interrupt)) void pagefaultHandler(struct InterruptFrame *frame, unsigned int errorCode);
+__attribute__((interrupt)) void doubleFault(struct InterruptFrame *frame, unsigned int errorCode);
